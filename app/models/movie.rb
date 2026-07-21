51dcb74a51fdaa6358a381f3bdfe32c4d6bcce8e
Adriana Vergara <adriana.b.vergara@berkeley.edu>
2026-07-21 22:41:28 +0000

    filter

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,10 +7,10 @@ class Movie < ApplicationRecord
   # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
   #  movies with those ratings
   # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.nil?
-      all
-    else
+    if ratings_list.nil? == false
       where(rating: ratings_list)
+    else
+      all
     end
   end
 end
